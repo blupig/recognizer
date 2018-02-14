32f5d4fa3afb1265fa35acefe753b3f36cc71486
Yunzhu Li <contact@yunzhu.li>
2018-02-14 18:29:37 -0500

    update learning rate

--- tiny-imagenet/model.py
+++ tiny-imagenet/model.py
@@ -79,7 +79,7 @@ def cnn_model_fn(features, labels, mode):
 
     # Configure the Training Op (for TRAIN mode)
     if mode == tf.estimator.ModeKeys.TRAIN:
-        optimizer = tf.train.AdamOptimizer()
+        optimizer = tf.train.AdamOptimizer(learning_rate=0.0001)
         train_op = optimizer.minimize(loss=loss, global_step=tf.train.get_global_step())
         return tf.estimator.EstimatorSpec(mode=mode, loss=loss, train_op=train_op)
 
