db2dc3ba3202d1f83ca5d8a9a7ec128135340616
Yunzhu Li <contact@yunzhu.li>
2018-02-13 23:52:32 -0500

    model update

--- tiny-imagenet/model.py
+++ tiny-imagenet/model.py
@@ -79,7 +79,7 @@ def cnn_model_fn(features, labels, mode):
 
     # Configure the Training Op (for TRAIN mode)
     if mode == tf.estimator.ModeKeys.TRAIN:
-        optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.0001)
+        optimizer = tf.train.AdamOptimizer()
         train_op = optimizer.minimize(loss=loss, global_step=tf.train.get_global_step())
         return tf.estimator.EstimatorSpec(mode=mode, loss=loss, train_op=train_op)
 
